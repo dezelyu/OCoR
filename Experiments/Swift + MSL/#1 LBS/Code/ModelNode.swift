c595c7c1baa76ada9300721309d8bd5a80933466
dezelyu <143949139+dezelyu@users.noreply.github.com>
2024-03-10 17:48:46 -0400

    Extracted the skinning data!!

--- Experiments/Swift + MSL/#1 LBS/Code/ModelNode.swift
+++ Experiments/Swift + MSL/#1 LBS/Code/ModelNode.swift
@@ -1,5 +1,10 @@
 import SceneKit
 class ModelNode: SCNNode {
+    struct SkinningData {
+        var boneIndices: vector_int8
+        var boneWeights: vector_float8
+    }
+    var skinningData: [SkinningData]!
     var vertices: [vector_float3]!
     var normals: [vector_float3]!
     var indices: [UInt32]!
@@ -7,6 +12,7 @@ class ModelNode: SCNNode {
     var normalInputBuffer: MTLBuffer!
     var vertexOutputBuffer: MTLBuffer!
     var normalOutputBuffer: MTLBuffer!
+    var skinningDataBuffer: MTLBuffer!
     var meshNode: SCNNode!
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -18,12 +24,22 @@ class ModelNode: SCNNode {
         self.initializeMeshNode()
     }
     func initializeMeshData() {
+        var skinningData = [SkinningData]()
         var vertices = [vector_float3]()
         var normals = [vector_float3]()
         var indices = [UInt32]()
         let modelScene = SCNScene(named: "Assets.scnassets/Model.scn")!
         let meshNode = modelScene.rootNode.childNode(withName: "mesh", recursively: false)!
         let meshGeometry = meshNode.geometry!
+        let skinner = meshNode.skinner!
+        var skinnerBoneIndices = [[Int32]]()
+        var skinnerBoneWeights = [[Float]]()
+        skinner.process(influenceCount: 8, boneIndices: &skinnerBoneIndices, boneWeights: &skinnerBoneWeights)
+        for vertexIndex in 0..<skinnerBoneIndices.count {
+            let boneIndices = vector_int8(skinnerBoneIndices[vertexIndex])
+            let boneWeights = vector_float8(skinnerBoneWeights[vertexIndex])
+            skinningData.append(SkinningData(boneIndices: boneIndices, boneWeights: boneWeights))
+        }
         for source in meshGeometry.sources {
             if (source.semantic == .vertex) {
                 source.process(input: Float.self, output: vector_float3.self, array: &vertices)
@@ -33,6 +49,7 @@ class ModelNode: SCNNode {
         }
         let meshElement = meshGeometry.elements.first!
         meshElement.process(indices: &indices)
+        self.skinningData = skinningData
         self.vertices = vertices
         self.normals = normals
         self.indices = indices
@@ -40,11 +57,13 @@ class ModelNode: SCNNode {
     func initializeMeshBuffers() {
         let device = MTLCreateSystemDefaultDevice()!
         let length = min(self.vertices.count, self.normals.count) * MemoryLayout<vector_float3>.size
+        let skinningDataBufferLength = self.skinningData.count * MemoryLayout<ModelNode.SkinningData>.size
         let options = MTLResourceOptions.cpuCacheModeWriteCombined
         self.vertexInputBuffer = device.makeBuffer(bytes: self.vertices, length: length, options: options)!
         self.normalInputBuffer = device.makeBuffer(bytes: self.normals, length: length, options: options)!
         self.vertexOutputBuffer = device.makeBuffer(bytes: self.vertices, length: length, options: options)!
         self.normalOutputBuffer = device.makeBuffer(bytes: self.normals, length: length, options: options)!
+        self.skinningDataBuffer = device.makeBuffer(bytes: self.skinningData, length: skinningDataBufferLength, options: options)!
     }
     func initializeMeshNode() {
         let vertexSource = SCNGeometrySource(vertexCount: self.vertices.count, vertexBuffer: self.vertexOutputBuffer)
